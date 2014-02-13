b123bd724ca7dd426d802f537b3427af6f5643dd
Mike Carifio <mike@carifio.org>
2014-02-13 17:33:58 -0500

    install/connect.zsh fixes a typo, adds comments to ~/.zsh{env,rc}.

--- install/connect.zsh
+++ install/connect.zsh
@@ -1,10 +1,25 @@
 #!/usr/bin/zsh
 
+# Connect ~/zshenv to your login environment by sourcing additional scripts in ~/.zsh{env,rc}.
+
+here=$(readlink -f $(dirname $0))
+
 cat <EOF >> ~/.zshenv
-[[ -r ~/zshenv/zshenv.epilog ]] && source ~/zshenv/zshenv.epilog
+
+# Added by $0 on $(date)
+# Source ~/zshenv/zshenv.epilog.zsh iff it exists.
+[[ -r ~/zshenv/zshenv.epilog ]] && source ~/zshenv/zshenv.epilog.zsh
 EOF
 
 
 cat <EOF >> ~/.zshrc
-[[ -r ~/zshenv/zshrc.epilog ]] && source ~/zshenv/zshrc.epilog
+
+# Added by $0 on $(date)
+# Source ~/zshenv/zshrc.epilog.zsh iff it exists.
+[[ -r ~/zshenv/zshrc.epilog ]] && source ~/zshenv/zshrc.epilog.zsh
+
 EOF
+
+
+# Make local working storage. Not yet sure this is really necessary.
+mkdir --parent $here/{tmp,var/log}
